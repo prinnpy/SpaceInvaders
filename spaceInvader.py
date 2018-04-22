d33642cb6be0979dbf4d6358bc404c26358128d2
Prinn Prinyanut <Prinn@Prinns-MacBook-Pro.local>
2018-04-22 15:45:41 -0600

    added border and ship

--- spaceInvader.py
+++ spaceInvader.py
@@ -1,9 +1,35 @@
 import turtle
 import os
 
+# screen setup
 wn = turtle.Screen()
 wn.bgcolor('black')
 wn.title('Space Invader')
+
+# border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color('white')
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+
+for side in range(4):
+	border_pen.fd(600)
+	border_pen.lt(90)
+
+border_pen.hideturtle()
+
+# player turtle
+player = turtle.Turtle()
+player.color('blue')
+player.shape('triangle')
+player.penup()
+player.speed(0)
+player.setposition(0,-250)
+player.setheading(90)
+
 
 
 
